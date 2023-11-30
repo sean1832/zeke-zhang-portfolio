d49f9d3c7eb4fcfa836fc1f759281199de7153b4
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-11-30 21:59:12 +1100

    feat: add decode effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { Circle } from "../assets/icons";
 import styles from "../style";
-import { TextDecode } from "./SubComponents";
+import { TextDecode, TextDecodeCycle } from "./SubComponents";
+import { motion } from "framer-motion";
 
 const smallText =
   "xl:text-[35px] sm:text-[25px] ss:text-[18px] text-[16px] text-white";
@@ -10,38 +11,43 @@ const Hero = () => {
     <div
       className={`flex justify-start items-center xl:py-[15%] lg:py-[25%] py-[35%] ${styles.lineItemsPaddingX}`}
     >
-      <div className="flex flex-col items-start sm:gap-3 gap-2">
+      <div className="flex flex-col items-start">
         <div className="flex gap-4 justify-start items-center">
-          <img src={Circle} className="lg:scale-50 scale-[0.3]" />
+          <motion.img
+            src={Circle}
+            className="lg:scale-50 scale-[0.3]"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{
+              duration: 1,
+              delay: 1,
+              ease: "easeInOut",
+              type: "spring",
+              stiffness: 200,
+            }}
+          />
           <h1 className="xl:text-[256px] lg:text-[128px] ss:text-[92px] text-[38px] text-white">
             <TextDecode
-              text="ZEKE ZHANG"
+              text="ZEKE ZHANG_"
               enableBlockChar={true}
               interval={80}
             />
           </h1>
         </div>
 
-        <div className="flex flex-col lg:gap-3 gap-0">
-          <div className="flex lg:gap-12 gap-4 justify-start items-center">
-            <img src={Circle} className="lg:scale-50 scale-[0.3]" />
-            <p className={`${smallText}`}>DESIGN</p>
-          </div>
-
-          <div className="flex lg:gap-12 gap-4 justify-start items-center">
-            <img src={Circle} className="lg:scale-50 scale-[0.3]" />
-            <p className={`${smallText}`}>CODE</p>
-          </div>
-
-          <div className="flex lg:gap-12 gap-4 justify-start items-center">
-            <img src={Circle} className="lg:scale-50 scale-[0.3]" />
-            <p className={`${smallText}`}>ANIMATE</p>
-          </div>
-
-          <div className="flex lg:gap-12 gap-4 justify-start items-center">
-            <img src={Circle} className="lg:scale-50 scale-[0.3]" />
-            <p className={`${smallText}`}>RESEARCH</p>
-          </div>
+        <div className="flex flex-col px-[60px]">
+          <p className={`${smallText} font-bold `}>
+            <TextDecodeCycle
+              texts={[
+                "ARCHITECTURAL DESIGNER_",
+                "COMPUTATIONAL DESIGNER_",
+                "OPENSOURCED DEVELOPER_",
+                "ANIMATOR_",
+              ]}
+              interval={30}
+              cycleInterval={1500}
+            />
+          </p>
         </div>
       </div>
     </div>
