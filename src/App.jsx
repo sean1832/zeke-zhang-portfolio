cb44ab5325c8f4e5c656919eb56ee0e95e9431f1
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-12-04 19:13:26 +1100

    refactor: create `Home` component for modularity

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
-import styles from "./style";
-import { useState, useCallback } from "react";
-import { Hero, Navbar, ProjectSection } from "./components";
-import { Cursor } from "./components/SubComponents";
-import { CursorContext } from "./util/CursorContext";
+import { Home } from "./pages";
 
 const App = () => {
-  const [cursorVariant, setCursorVariant] = useState("default");
-  const updateCursorVariant = useCallback((variant) => {
-    setCursorVariant(variant);
-  }, []);
-
   return (
-    <CursorContext.Provider value={{ updateCursorVariant }} className="!scroll-smooth">
-      <div className="bg-primary w-full overflow-hidden">
-        <Cursor cursorVariant={cursorVariant} />
-        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Navbar />
-          </div>
-        </div>
-
-        <div className="bg-primary relative"></div>
-        <div className={`${styles.flexStart} z-10`}>
-          {/* overlay components */}
-          <div className={`${styles.boxWidth}`}>
-            <Hero />
-          </div>
-        </div>
-        <div>
-          <ProjectSection />
-        </div>
-      </div>
-    </CursorContext.Provider>
+    <div>
+      <Home />
+    </div>
   );
 };
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import { useState, useCallback } from "react";
+import { Hero, Navbar, ProjectSection } from "../components";
+import { Cursor } from "../components/SubComponents";
+import { CursorContext } from "../util/CursorContext";
+import styles from "../style";
+
+const Home = () => {
+  const [cursorVariant, setCursorVariant] = useState("default");
+  const updateCursorVariant = useCallback((variant) => {
+    setCursorVariant(variant);
+  }, []);
+  return (
+    <CursorContext.Provider value={{ updateCursorVariant }} className="!scroll-smooth">
+      <div className="bg-primary w-full overflow-hidden">
+        <Cursor cursorVariant={cursorVariant} />
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+
+        <div className="bg-primary relative"></div>
+        <div className={`${styles.flexStart} z-10`}>
+          {/* overlay components */}
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+        <div>
+          <ProjectSection />
+        </div>
+      </div>
+    </CursorContext.Provider>
+  );
+};
+
+export default Home;
