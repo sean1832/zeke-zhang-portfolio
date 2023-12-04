242e3f5b7b12aa72752d81fb8f7d87843eb2d23c
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-12-04 18:30:40 +1100

    chore: update index image extension

--- src/assets/hero/index.js
+++ src/assets/hero/index.js
@@ -1,10 +1,10 @@
-import BasketballClub from "./BasketballClub.jpeg";
-import Dharavi from "./Dharavi.jpeg";
-import Hereditary from "./Hereditary.jpeg";
-import Kaira from "./Kaira.jpeg";
-import Modular from "./Modular.jpeg";
-import OnCountry from "./OnCountry.jpeg";
-import AISuperblock from "./AISuperblock.png";
-import AISuperblock2 from "./AISuperblock_2.png";
+import BasketballClub from "./BasketballClub.webp";
+import Dharavi from "./Dharavi.webp";
+import Hereditary from "./Hereditary.webp";
+import Kaira from "./Kaira.webp";
+import Modular from "./Modular.webp";
+import OnCountry from "./OnCountry.webp";
+import AISuperblock from "./AISuperblock.webp";
+import AISuperblock2 from "./AISuperblock_2.webp";
 
 export { BasketballClub, Dharavi, Hereditary, Kaira, Modular, OnCountry, AISuperblock, AISuperblock2 };
