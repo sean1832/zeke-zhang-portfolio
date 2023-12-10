441d988268001bac95b6f68b315df81416734df0
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-12-11 05:37:37 +1100

    feat: implement basic skeleton modal

--- src/pages/projects/SharaClarke/ArchieRoachModal.jsx
+++ src/pages/projects/SharaClarke/ArchieRoachModal.jsx
@@ -7,6 +7,20 @@ const ArchieRoachModal = () => {
     <div className="w-[1200px] flex flex-col gap-6">
       <h1>{formatText(archieRoach.name, "ALL_CAPS")}</h1>
       <p>{archieRoach.paragraphs.intro}</p>
+      {/* body */}
+      <div className="flex gap-5 items-center justify-center">
+        <div className="flex-none daisy-skeleton w-[500px] h-[500px]" />
+        <p className="p-20">{archieRoach.paragraphs.groundFloor}</p>
+      </div>
+      <div className="flex gap-5 items-center justify-center">
+        <p className="p-20">{archieRoach.paragraphs.firstSecondFloor}</p>
+        <div className="flex-none daisy-skeleton w-[500px] h-[500px]" />
+      </div>
+
+      <div className="daisy-skeleton w-full h-[500px]" />
+      <p>{archieRoach.paragraphs.interior}</p>
+      <div className="daisy-skeleton w-full h-[500px]" />
+      <p>{archieRoach.paragraphs.spaceArrangement}</p>
     </div>
   );
 };
